a4006ce8de82d28bfbcea5a275bce2a398dfff9f
gabriel meireles <gabriel.meireles@youdb.com.br>
2019-07-27 23:55:46 -0300

    criando pagina de cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+
+  appRoutes: Routes=[
+    { path: 'create-account', component: RegisterComponent },
+    { path: '', component: LoginComponent },
+  ]
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material';
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { RegisterComponent } from './register/register.component'
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+let routes = new AppRoutingModule().appRoutes;
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      routes
+    ),
     MatButtonModule,
     MatInputModule,
     MatSelectModule,
